a25a61f20bde28f6d4bf64ce8eb54236a71c2d85
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-07 19:56:08 +0900

    [refactor/category] CategoryViewModel.kt DI

--- app/src/main/java/com/example/searchplacement/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/example/searchplacement/viewmodel/CategoryViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.searchplacement.data.store.StoreResponse
 import com.example.searchplacement.repository.StoreRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class CategoryViewModel@Inject constructor(
     private val storeRepository: StoreRepository
 ) : ViewModel() {
