e860d4f7f2e363a99cd88e9f0b78f51d9ef2951c
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-12 01:40:49 +0900

    [refactor/topBar] topBar UI 제거

--- app/src/main/java/com/example/searchplacement/activity/MainActivity.kt
+++ app/src/main/java/com/example/searchplacement/activity/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.searchplacement.activity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.searchplacement.BuildConfig
 import com.example.searchplacement.ui.theme.SearchPlacementTheme
 import com.example.searchplacement.ui.user.home.MainView
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("MainActivity", "BASE_URL = ${BuildConfig.BASE_URL}")
         setContent {
             SearchPlacementTheme {
                 MainView()
--- app/src/main/java/com/example/searchplacement/ui/user/home/MainView.kt
+++ app/src/main/java/com/example/searchplacement/ui/user/home/MainView.kt
@@ -5,43 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.searchplacement.navigation.MainBottomNavItem
 import com.example.searchplacement.navigation.MainNavigation
-import com.example.searchplacement.viewmodel.FavoriteViewModel
+import com.example.searchplacement.ui.theme.White
 import com.example.searchplacement.viewmodel.MainViewModel
-import com.example.searchplacement.viewmodel.MenuSectionViewModel
-import com.example.searchplacement.viewmodel.MenuViewModel
-import com.example.searchplacement.viewmodel.PlacementViewModel
-import com.example.searchplacement.viewmodel.StoreViewModel
 
 
 @Composable
 fun MainView() {
     val navController = rememberNavController()
     val mainViewModel: MainViewModel = hiltViewModel()
-    val storeViewModel: StoreViewModel = hiltViewModel()
-    val favoriteViewModel: FavoriteViewModel = hiltViewModel()
-    val menuViewModel: MenuViewModel = hiltViewModel()
-    val menuSectionViewModel: MenuSectionViewModel = hiltViewModel()
-    val placementViewModel: PlacementViewModel = hiltViewModel()
     Scaffold(
-        modifier = Modifier.background(Color.White),
-        topBar = {
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute = navBackStackEntry?.destination?.route
-            val items = MainBottomNavItem.items.map { it.screenRoute }
-
-            if (items.contains(currentRoute)) {
-                val currentItem = MainBottomNavItem.items.find { it.screenRoute == currentRoute }
-                MainTopBar(title = currentItem?.title ?: "")
-            }
-        },
+        modifier = Modifier.background(White),
         bottomBar = {
             MainBottomBar(
                 Color.White, Color.Black,
@@ -52,9 +30,9 @@ fun MainView() {
         Column(
             Modifier
                 .padding(innerPadding)
-                .background(Color.White)
+                .background(White)
         ) {
-            MainNavigation(navController, mainViewModel, storeViewModel, favoriteViewModel,menuViewModel,menuSectionViewModel,placementViewModel)
+            MainNavigation(navController, mainViewModel)
         }
     }
 }
