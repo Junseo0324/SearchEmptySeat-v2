fa0f829f4e63e6b8cf2e3b208d9befb306c3ff43
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-19 16:00:28 +0900

    [refactor/bottom_nav] BottomNavigation 아이콘 변경

--- app/src/main/java/com/example/searchplacement/navigation/OwnerBottomNavItem.kt
+++ app/src/main/java/com/example/searchplacement/navigation/OwnerBottomNavItem.kt
@@ -1,12 +1,12 @@
 package com.example.searchplacement.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Event
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Restaurant
 import androidx.compose.material.icons.filled.Store
-import androidx.compose.material.icons.outlined.Event
-import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.filled.TableRestaurant
+import androidx.compose.material.icons.outlined.Restaurant
 import androidx.compose.material.icons.outlined.Store
+import androidx.compose.material.icons.outlined.TableRestaurant
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class OwnerBottomNavItem(
@@ -15,7 +15,7 @@ sealed class OwnerBottomNavItem(
     val filledIcon: ImageVector,
     val screenRoute: String
 ) {
-    object Home : OwnerBottomNavItem("매장 관리", Icons.Outlined.Home, Icons.Filled.Home, "owner_home")
-    object Store : OwnerBottomNavItem("메뉴 관리", Icons.Outlined.Store, Icons.Filled.Store, "owner_store")
-    object Reservation : OwnerBottomNavItem("예약 관리", Icons.Outlined.Event, Icons.Filled.Event, "owner_reserve")
+    object Home : OwnerBottomNavItem("매장 관리", Icons.Outlined.Store, Icons.Filled.Store, "owner_home")
+    object Store : OwnerBottomNavItem("메뉴 관리", Icons.Outlined.Restaurant, Icons.Filled.Restaurant, "owner_store")
+    object Reservation : OwnerBottomNavItem("예약 관리", Icons.Outlined.TableRestaurant, Icons.Filled.TableRestaurant, "owner_reserve")
 }
