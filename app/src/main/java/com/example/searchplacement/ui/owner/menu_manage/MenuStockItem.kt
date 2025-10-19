464bce15f17f0bf6bb0b585613c9fda1b75ae401
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-19 16:33:16 +0900

    [refactor/edit_menu] EditMenu UI 변경

---
+++ app/src/main/java/com/example/searchplacement/ui/owner/menu_manage/MenuStockItem.kt
@@ -0,0 +1,81 @@
+package com.example.searchplacement.ui.owner.menu_manage
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.searchplacement.data.menu.MenuResponse
+import com.example.searchplacement.di.AppModule
+import com.example.searchplacement.ui.theme.Dimens
+import com.example.searchplacement.ui.theme.IconColor
+import com.example.searchplacement.ui.theme.White
+import com.example.searchplacement.ui.utils.rememberImageLoaderWithToken
+
+@Composable
+fun MenuStockItem(
+    menu: MenuResponse,
+    isAvailable: Boolean,
+    onToggle: (Boolean) -> Unit
+) {
+    val imageLoader = rememberImageLoaderWithToken()
+    val imageUrl = menu.image?.firstOrNull()
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp)
+    ) {
+
+        if (!imageUrl.isNullOrEmpty()) {
+            AsyncImage(
+                model = "${AppModule.BASE_URL}/api/files/$imageUrl",
+                imageLoader = imageLoader,
+                contentDescription = menu.name,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+        } else {
+            Box(
+                Modifier
+                    .size(60.dp)
+                    .clip(RoundedCornerShape(Dimens.Default))
+                    .background(White),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("No Img", fontSize = 12.sp, color = IconColor)
+            }
+        }
+
+        Column(Modifier
+            .weight(1f)
+            .padding(10.dp)) {
+            Text(menu.name, fontWeight = FontWeight.SemiBold)
+            Text("${menu.price}원", color = IconColor, fontSize = 15.sp)
+            Text(menu.description, fontSize = 13.sp, maxLines = 2, overflow = TextOverflow.Ellipsis)
+        }
+
+        MultiToggleSwitch(
+            labels = listOf("판매", "품절"),
+            selectedIndex = if (isAvailable) 0 else 1,
+            onToggle = { index -> onToggle(index == 0) },
+            modifier = Modifier.width(100.dp)
+        )
+
+    }
+}
